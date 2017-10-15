3561302f409e8b3573c72afeefdd417f2b6adf0e
Caitlin Boeyens <caitieb@underware.co.za>
2017-10-15 18:06:55 +0200

    carousel complete
    Content is still static

---
+++ js/home.js
@@ -0,0 +1,17 @@
+$(function(){
+$('.owl-carousel').owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    autoplaySpeed:500,
+    nav:false,
+    dotsEach:1
+});
+
+     $('[data-toggle="tooltip"]').tooltip();
+});
+
+
