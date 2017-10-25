d4d881874d042929e9d7f536f5becd60418b0402
Caitlin Boeyens <caitieb@underware.co.za>
2017-10-25 22:15:02 +0200

    Filters designed
    I used quite a few plugins for this to work

--- js/library.js
+++ js/library.js
@@ -1,17 +1,23 @@
 $(function(){
+
+    // $(function() {
+    //     $("input").autocomplete({
+    //         source:[films.type]
+    //     }); 
+    // });
     
     $(function appendAllMovies()
     {
         $(films).each(function(){
             $("#all_movie_container").append(`
             <div class="movie col-xs-12 col-sm-4">
-                <div id="${this.id}" class="movie-block">
+                <div id="${this.id}" class="movie-block" data-year="${this.year}" data-genre="${this.type}" data-imdb="${this.Imdb}" data-reels="${this.reels}">
                     <div class="overlay">
                         <div class="overlay-name">
                             <h4>${this.name}</h4>
                         </div>
                         <div class="overlay-info">
-                            <h5>${this.type} | ${this.year} | IMDb: ${this.Imdb}/10 | 4 Reels</h5>
+                            <h5>${this.type} | ${this.year} | IMDb: ${this.Imdb}/10 | ${this.reels} Reels</h5>
                             <p>${this.description}</p>
                         </div>
                         <div class="overlay-buttons">
@@ -32,4 +38,32 @@ $(function(){
             $(`#${this.id}`).css("background-image", `url("${this['img-path'] }")`);
         });
     });
+
+    // RATINGS
+
+        // // target element
+        // var el = $;
+
+        // // current rating, or initial rating
+        // var currentRating = 0;
+
+        // // max rating, i.e. number of stars you want
+        // var maxRating= 5;
+
+        // // callback to run after setting the rating
+        // var callback = function(rating) { alert(rating); };
+
+        // // rating instance
+        // var myRating = rating(el, currentRating, maxRating, callback);
+
+        // YEAR
+        
+        $("#ex16b").slider({ 
+            min: 0, 
+            max: 6, 
+            value: [0, 6],
+            focus: true,
+            ticks: [0, 1, 2, 3, 4, 5],
+            ticks_labels: ['Past', '1980', '1990', '2000', '2010', 'Future'],
+            ticks_snap_bounds: 30 });
 });
