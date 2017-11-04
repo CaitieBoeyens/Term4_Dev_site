e422df31b3a9a0861771ec83a4733bf6e9d27894
tiffvan <32732343+tiffvan@users.noreply.github.com>
2017-11-04 09:38:05 +0200

    watchpage video
    styling halfway

--- js/watchpage.js
+++ js/watchpage.js
@@ -1,1 +1,97 @@
+var vid, playBtn, seekslider, curTimeText, durTimeText, muteBtn, volumeslider, fullscreenBtn;
 
+function initializePlayer() {
+    // Set object references
+    vid = document.getElementById("myVideo");
+    playBtn = document.getElementById("playPauseBtn");
+    seekslider = document.getElementById("seekslider");
+    curTimeText = document.getElementById("curTimeText");
+    durTimeText = document.getElementById("durTimeText");
+    muteBtn = document.getElementById("muteBtn");
+    volumeslider = document.getElementById("volumeslider");
+    fullscreenBtn = document.getElementById("fullscreenBtn");
+    
+    // Add event listeners
+    playBtn.addEventListener("click", playPause, false);
+    seekslider.addEventListener("change", vidSeek, false);
+    vid.addEventListener("timeupdate", seekTimeUpdate, false);
+    muteBtn.addEventListener("click", vidMute, false);
+    volumeslider.addEventListener("change", setVolume, false);
+    fullscreenBtn.addEventListener("click", toggleFullscreen, false);
+}
+
+window.onload = initializePlayer;
+
+
+function playPause() {
+    
+    if(vid.paused) {
+        vid.play();
+        playBtn.style.background = "url(../assets/pause.png)";
+    } else {
+        vid.pause();
+        playBtn.style.background = "url(../assets/play.svg)";
+    }
+    
+};
+
+function vidSeek() {
+    
+    var seekto = vid.duration * (seekslider.value / 100);
+    vid.currentTime = seekto;
+    
+}
+
+function seekTimeUpdate() {
+    
+    var newTime = vid.currentTime * (100 / vid.duration);
+    seekslider.value = newTime;
+    
+    var curmins = Math.floor(vid.currentTime / 60);
+    var cursecs = Math.floor(vid.currentTime - curmins * 60);
+    var durmins = Math.floor(vid.duration / 60);
+    var dursecs = Math.round(vid.duration - durmins * 60);
+    
+    if(cursecs < 10) {
+        cursecs = "0"+cursecs;
+    }
+    if(curmins < 10) {
+        curmins = "0"+curmins;
+    }
+    if(dursecs < 10) {
+        dursecs = "0"+dursecs;
+    }
+    if(durmins < 10) {
+        durmins = "0"+durmins;
+    }
+    
+    curTimeText.innerHTML = curmins+":"+cursecs;
+    durTimeText.innerHTML = durmins+":"+dursecs;
+}
+
+function vidMute() {
+    
+    if(vid.muted) {
+        vid.muted = false;
+        muteBtn.innerHTML = "Mute";
+    } else {
+        vid.muted = true;
+        muteBtn.innerHTML = "Unmute";
+    }
+}
+
+function setVolume() {
+    
+    vid.volume = volumeslider.value / 100;
+}
+
+function toggleFullscreen() {
+    
+    if(vid.requestFullscreen) {
+       vid.requestFullscreen();
+    } else if(vid.webkitRequestFullscreen) {
+        vid.webkitRequestFullscreen();
+    } else if(vid.mozRequestFullscreen) {
+         vid.mozRequestFullscree();     
+    }
+}
