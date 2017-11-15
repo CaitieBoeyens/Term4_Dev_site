49aeeb526f2ee878459f71c253697ce393640996
Armand Pretorius <170045@virtualwindow.co.za>
2017-11-15 22:34:29 +0200

    "Watchlist" Button Change Working Code

--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
 $(function(){
     $(".add_to_watchlist").click(function(){
-        console.log("Hellllooooo");
-        if($(this).data("tooltip") === "Add to Watchlist"){
+
+        if($(this).attr("data-tooltip") === "Add to Watchlist"){
             $(this).attr("data-tooltip", "Remove from Watchlist");
             $(this).children("img").css("transform", "rotate(45deg)");
-            console.log($(this).data("tooltip"));
-
         } else {
             $(this).attr("data-tooltip", "Add to Watchlist");
             $(this).children("img").css("transform", "rotate(0deg)");
-            console.log("lol k");
         } ;
-
-        
-        
-    
-        
-        
     });
 });
