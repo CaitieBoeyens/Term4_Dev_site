426e7b7825edecefacd6e5b4d2d4f1569b6313b0
Armand Pretorius <170045@virtualwindow.co.za>
2017-11-15 13:02:27 +0200

    Placed films on Watchlist with remove function.

--- js/watchlist.js
+++ js/watchlist.js
@@ -1,19 +1,34 @@
 $(function(){
 
-    // $(function() {
-    //     $("input").autocomplete({
-    //         source:[films.type]
-    //     }); 
-    // });
-    
+console.log("ready");
 
-
-
-$("#remove-btn").click(function(){
-           $(this).parent(".movie").remove();
-           
+$("#swap1").click(function(){
+    $(this).parent(".overlay-buttons").parent(".overlay").parent(".movie-block1").parent("#poster1").fadeOut(400, function(){
+           $(this).remove();
+        });
        });
 
+       $("#swap2").click(function(){
+        $(this).parent(".overlay-buttons").parent(".overlay").parent(".movie-block2").parent("#poster2").fadeOut(400, function(){
+               $(this).remove();
+            });
+           });
+
+           $("#swap3").click(function(){
+            $(this).parent(".overlay-buttons").parent(".overlay").parent(".movie-block3").parent("#poster3").fadeOut(400, function(){
+                   $(this).remove();
+                });
+               });
+               $("#swap4").click(function(){
+                $(this).parent(".overlay-buttons").parent(".overlay").parent(".movie-block4").parent("#poster4").fadeOut(400, function(){
+                       $(this).remove();
+                    });
+                   });
+$("#swap5").click(function(){
+    $(this).parent(".overlay-buttons").parent(".overlay").parent(".movie-block5").parent("#poster5").fadeOut(400, function(){
+            $(this).remove();
+                });
+    });
 
     
 
